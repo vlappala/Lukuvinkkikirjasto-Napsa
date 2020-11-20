924012b336683c9ded939a81ca9e142b655aa434
sumuh <muhli.susanna@gmail.com>
2020-11-20 14:47:32 +0200

    lukuvinkki-rajapinta tehty, packageja lisätty, fork-testausta

---
+++ src/main/java/interfaces/Lukuvinkki.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface Lukuvinkki {
+    
+    String label = "";
+    
+    public void setLabel();
+    public String getLabel();
+    public String toString();
+    
+}
