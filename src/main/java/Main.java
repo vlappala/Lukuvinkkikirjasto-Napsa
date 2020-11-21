9f697d62e68befcd79176bff4dc3a7d379a33cdf
Akseli N <virtualakseli@gmail.com>
2020-11-21 16:08:27 +0200

    Lisättiin tiedostoon tallentaminen ja lukumahdollisuus.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,25 @@
 import java.util.*;
 import classes.Kirja;
+import io.ConsoleIO;
+import dao.LukuvinkkiDao;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Kirja abc = new Kirja("Aapinen");
+        ConsoleIO console = new ConsoleIO();
+        LukuvinkkiDao dao = new LukuvinkkiDao();
+        
 
-        System.out.println(abc.toString());
+        Kirja abc = new Kirja(console.readInput("Anna lukuvinkin otsikko: "));
+        console.printOutput("Luotiin lukuvinkki: " + abc.toString());
+
+        dao.saveToFile(abc.toString(), abc.toString());
+        
+
+
+
 
     }
 
