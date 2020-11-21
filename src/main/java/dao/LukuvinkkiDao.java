33d54ebc48be7427f827622d1a8d19a0db28d281
Akseli N <virtualakseli@gmail.com>
2020-11-21 17:05:44 +0200

    Dao mukaan

---
+++ src/main/java/dao/LukuvinkkiDao.java
@@ -0,0 +1,34 @@
+package dao;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LukuvinkkiDao {
+
+    String filePath = "./";
+    
+
+    public void saveToFile(String filename, String content) throws IOException {
+        FileWriter fileWriter = new FileWriter(filePath + filename + ".txt");
+        fileWriter.write(content);
+        fileWriter.close();
+    }
+
+    public String readFromFile(String filename) throws FileNotFoundException, IOException {
+        FileReader fileReader = new FileReader(filePath + filename + ".txt");
+        String fileContent = "";
+        int ch = fileReader.read();
+
+        while(ch != -1) {
+            fileContent = fileContent + (char)ch;
+            fileReader.close();
+        }
+
+        return fileContent;
+
+    }
+
+}
