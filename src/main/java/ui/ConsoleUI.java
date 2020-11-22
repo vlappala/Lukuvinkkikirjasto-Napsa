87c52c0a6662f982daaf5fb34d809cf2ab243386
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-11-22 19:32:27 +0200

    Petrin UI

---
+++ src/main/java/ui/ConsoleUI.java
@@ -0,0 +1,29 @@
+package ui;
+
+import classes.Kirja;
+import dao.LukuvinkkiDao;
+import io.ConsoleIO;
+
+public class ConsoleUI {
+
+    private final String FILE = "Vinkit";
+    private ConsoleIO console;
+    private LukuvinkkiDao dao;
+
+    public ConsoleUI(ConsoleIO console, LukuvinkkiDao dao) {
+        this.console = console;
+        this.dao = dao;
+    }
+
+    public void run() {
+        Kirja kirja = new Kirja(console.readInput("Anna lukuvinkin otsikko: "));
+        if (kirja.toString() != null && !kirja.toString().isEmpty()) {
+            try {
+                dao.saveToFile(FILE, kirja.toString());
+            } catch (Exception e) {
+                console.printOutput("VIRHE: " + e.getMessage());
+            }
+            console.printOutput("Luotiin lukuvinkki: " + kirja.toString());
+        }
+    }
+}
---
+++ src/test/java/ui/ConsoleUITest.java
@@ -0,0 +1,47 @@
+package ui;
+
+import dao.LukuvinkkiDao;
+import io.ConsoleIO;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+public class ConsoleUITest {
+
+    private final String FILE = "Vinkit";
+
+    ConsoleIO console;
+    LukuvinkkiDao dao;
+    ConsoleUI ui;
+
+    @Before
+    public void setUp() {
+        console = mock(ConsoleIO.class);
+        dao = mock(LukuvinkkiDao.class);
+        ui = new ConsoleUI(console, dao);
+    }
+
+    @Test
+    public void uiKysyyVinkin() {
+        ui.run();
+        verify(console).readInput("Anna lukuvinkin otsikko: ");
+    }
+
+    @Test
+    public void uiTallentaaVinkin() {
+        when(console.readInput("Anna lukuvinkin otsikko: ")).thenReturn("Raamattu");
+        ui.run();
+        try {
+            verify(dao).saveToFile(FILE, "Raamattu");
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void uiTulostaaVinkin() {
+        when(console.readInput("Anna lukuvinkin otsikko: ")).thenReturn("Raamattu");
+        ui.run();
+        verify(console).printOutput("Luotiin lukuvinkki: Raamattu");
+    }
+}
