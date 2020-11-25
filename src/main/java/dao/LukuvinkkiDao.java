3eb6c0836cc239758f4462ba149137ae16a3ff32
Akseli N <virtualakseli@gmail.com>
2020-11-25 21:15:14 +0200

    Luotiin GSON:lla toimiva Kirja-olion kirjoitus tiedostoon, sekä Kirja-olio listan palautus tiedostosta.

--- src/main/java/dao/LukuvinkkiDao.java
+++ src/main/java/dao/LukuvinkkiDao.java
@@ -1,32 +1,52 @@
 package dao;
 
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.IOException;
+import java.lang.reflect.Type; 
+import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;  
+import java.util.Scanner;
+import java.util.List;
+
+
+import domain.Kirja;
 
 public class LukuvinkkiDao {
 
     String filePath = "./";
+    Gson gson = new Gson();
     
 
-    public void saveToFile(String filename, String content) throws IOException {
-        FileWriter fileWriter = new FileWriter(filePath + filename + ".txt", true);
-        fileWriter.write(content + "\n");
+    public void saveToFile(String filename, List<Kirja> content) throws IOException {
+        
+        String output = gson.toJson(content);
+        
+        FileWriter fileWriter = new FileWriter(filePath + filename + ".txt");
+        fileWriter.write(output + "\n");
+        
         fileWriter.close();
     }
 
-    public String readFromFile(String filename) throws FileNotFoundException, IOException {
-        FileReader fileReader = new FileReader(filePath + filename + ".txt");
-        String fileContent = "";
-        int ch = fileReader.read();
-
-        while(ch != -1) {
-            fileContent = fileContent + (char)ch;
-            fileReader.close();
+    public List<Kirja> readFromFile(String filename) throws FileNotFoundException, IOException {
+        
+        FileInputStream input = new FileInputStream(filePath + filename + ".txt");
+        Scanner scanner = new Scanner(input);
+       
+        String content = "";
+        
+        
+        
+        
+        while (scanner.hasNextLine()) {
+            
+            content = content + scanner.nextLine();
         }
 
+        scanner.close();
+
+        List<Kirja> fileContent = gson.fromJson(content, new TypeToken<List<Kirja>>() {}.getType());
+
+        
         return fileContent;
 
     }
--- src/main/java/ui/ConsoleUI.java
+++ src/main/java/ui/ConsoleUI.java
@@ -1,29 +1,62 @@
+
 package ui;
 
 import domain.Kirja;
+import domain.Lukuvinkki;
 import dao.LukuvinkkiDao;
 import io.ConsoleIO;
+import java.util.List;
+import java.util.Arrays;
 
 public class ConsoleUI {
 
     private final String FILE = "Vinkit";
     private ConsoleIO console;
     private LukuvinkkiDao dao;
+    private String uusiLukuvinkki;
+    private Kirja[] content;
+    private List<Kirja> vinkit;
+    
+    
 
     public ConsoleUI(ConsoleIO console, LukuvinkkiDao dao) {
         this.console = console;
         this.dao = dao;
+        
+       
+        
     }
 
     public void run() {
         Kirja kirja = new Kirja(console.readInput("Anna lukuvinkin otsikko: "));
+        String loppuTulostus = "";
+        
         if (kirja.toString() != null && !kirja.toString().isEmpty()) {
             try {
-                dao.saveToFile(FILE, kirja.toString());
+
+                    //Ensin luetaan Vinkit.txt, vanhat tiedot otetaan talteen listaan
+                    vinkit = dao.readFromFile(FILE);
+
+                    //Uusi olio lisätään listaan
+                    vinkit.add(kirja);
+
+                    //Lista työnnetään JSON-muodossa tiedostoon, samalla ylikirjoittaen
+                    dao.saveToFile(FILE, vinkit);
+
+                    //Kikkailua
+                    int vika = dao.readFromFile(FILE).size() - 1;
+                    loppuTulostus = dao.readFromFile(FILE).get(vika).toString();
+
+                    
+                
+                
+               
+
             } catch (Exception e) {
                 console.printOutput("VIRHE: " + e.getMessage());
             }
-            console.printOutput("Luotiin lukuvinkki: " + kirja.toString());
+            
+            console.printOutput("Luotiin lukuvinkki: " + loppuTulostus);
         }
     }
     
